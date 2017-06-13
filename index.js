c7920e6d30f75cb9d4a5d8e6d235b0ee3d2a2936
tony <tony@linktime.cloud>
2017-06-13 17:51:42 +0800

    fix bug

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ function parseObject(obj, currentIndent) {
     for (let key in obj) {
         if (isArray(obj[key])) {
             if (isPureArray(obj[key])) {
-                tmp.push({output: key + ':' + parsePureArray(obj[key]), indent})
+                tmp.push({text: key + ':' + parsePureArray(obj[key]), indent})
             } else {
-                tmp.push({output: key + ':', indent})
-                tmp.push({ouput: '[', indent})
+                tmp.push({text: key + ':', indent})
+                tmp.push({text: '[', indent})
                 tmp = tmp.concat(parseArray(obj[key], indent))
-                tmp.push({output: ']', indent})
+                tmp.push({text: ']', indent})
             }
         } else if (obj[key] instanceof Object) {
-            tmp.push({output: key + ': {', indent})
+            tmp.push({text: key + ': {', indent})
             tmp = tmp.concat(parseObject(obj[key], indent))
-            tmp.push({output: '}', indent})
+            tmp.push({text: '}', indent})
         } else {
             var str = key + ':' + obj[key]
-            tmp.push({output: str, indent})
+            tmp.push({text: str, indent})
         }
     }
     return tmp
@@ -38,18 +38,18 @@ function parseArray(arr, currentIndent) {
     arr.forEach((item) => {
         if (isArray(item)) {
             if (isPureArray(item)) {
-                tmp.push({output: parsePureArray(item), indent})
+                tmp.push({text: parsePureArray(item), indent})
             } else {
-                tmp.push({output: '[', indent})
+                tmp.push({text: '[', indent})
                 tmp = tmp.concat(parseArray(item, indent))
-                tmp.push({output: ']', indent})
+                tmp.push({text: ']', indent})
             } 
         } else if (item instanceof Object) {
-            tmp.push({output: '{', indent})
+            tmp.push({text: '{', indent})
             tmp = tmp.concat(parseObject(item, indent))
-            tmp.push({output:'}', indent})
+            tmp.push({text:'}', indent})
         } else {
-            tmp.push({output: item, indent})
+            tmp.push({text: item, indent})
         }
     });
     return tmp
@@ -91,13 +91,13 @@ function isArray(o) {
 module.exports = function parseJson(formdata) {
     var tmp = []
     if (isArray(formdata)) {
-        tmp.push({output: '[', indent: 0})
+        tmp.push({text: '[', indent: 0})
         tmp.concat(parseArray(formdata, 0))
-        tmp.push({output: ']', indent: 0})
+        tmp.push({text: ']', indent: 0})
     } if (formdata instanceof Object) {
-        tmp.push({output: '{', indent: 0})
+        tmp.push({text: '{', indent: 0})
         tmp = tmp.concat(parseObject(formdata, 0))
-        tmp.push({output: '}', indent: 0})
+        tmp.push({text: '}', indent: 0})
     }
     return tmp
 }
--- readme.md
+++ readme.md
@@ -2,32 +2,34 @@
 
 ##输出形式
 ```
-[ { output: '{', indent: 0 },
-  { output: 'a:1', indent: 4 },
-  { output: 'b:true', indent: 4 },
-  { output: 'c:null', indent: 4 },
-  { output: 'd: {', indent: 4 },
-  { output: 'dd:test me', indent: 8 },
-  { output: 'cd:', indent: 8 },
-  { ouput: '[', indent: 8 },
-  { output: 1, indent: 12 },
-  { output: 2, indent: 12 },
-  { output: 3, indent: 12 },
-  { output: 4, indent: 12 },
-  { output: '{', indent: 12 },
-  { output: 'bar:zz', indent: 16 },
-  { output: 'foo:help', indent: 16 },
-  { output: '}', indent: 12 },
-  { output: ']', indent: 8 },
-  { output: '}', indent: 4 },
-  { output: 'e:[1, 2, 3, 4, 5]', indent: 4 },
-  { output: '}', indent: 0 } ]
+[ { text: '{', indent: 0 },
+  { text: 'a:1', indent: 4 },
+  { text: 'b:true', indent: 4 },
+  { text: 'c:null', indent: 4 },
+  { text: 'd: {', indent: 4 },
+  { text: 'dd:test me', indent: 8 },
+  { text: 'cd:', indent: 8 },
+  { text: '[', indent: 8 },
+  { text: 1, indent: 12 },
+  { text: 2, indent: 12 },
+  { text: 3, indent: 12 },
+  { text: 4, indent: 12 },
+  { text: '{', indent: 12 },
+  { text: 'bar:zz', indent: 16 },
+  { text: 'foo:help', indent: 16 },
+  { text: '}', indent: 12 },
+  { text: ']', indent: 8 },
+  { text: '}', indent: 4 },
+  { text: 'e:[1, 2, 3, 4, 5]', indent: 4 },
+  { text: '}', indent: 0 } ]
 ```
 
 ##使用
 ```
 var parseJson = require('./index.js')
 var output = parseJson(someJsonData)
+
+参见demo.js
 ```
 
 ##注意
