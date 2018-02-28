947112301003099a403e662a00108f7452a338c6
tony <tony@linktime.cloud>
2018-02-28 18:53:05 +0800

    update

--- index.js
+++ index.js
@@ -1,174 +1,58 @@
-/**
- * 不关心外部"{}"
- * @param {object} obj
- * @param {number} currentIndent
- */
-function parseObject(obj, currentIndent) {
-    let tmp = []
-    let indent = currentIndent + 4
-    let i = 0
-    for (let key in obj) {
-        if (i < Object.keys(obj).length - 1) {
-            i++
-            // need comma
-            if (isArray(obj[key])) {
-                // item 为数组
-                if (isPureArray(obj[key])) {
-                    tmp.push({ text: `"${key}": ${parsePureArray(obj[key])},`, indent })
-                } else {
-                    tmp.push({ text: `"${key}": [`, indent })
-                    tmp = tmp.concat(parseArray(obj[key], indent))
-                    tmp.push({ text: '],', indent })
-                }
-            } else if (obj[key] instanceof Object) {
-                // item 为对象
-                tmp.push({ text: `"${key}": {`, indent })
-                tmp = tmp.concat(parseObject(obj[key], indent))
-                tmp.push({ text: '},', indent })
-            } else {
-                // item 为普通value
-                let str = ''
-                if (typeof obj[key] === 'string') {
-                    str = `"${key}": "${obj[key]}",`
-                } else {
-                    str = `"${key}": ${obj[key]},`
-                }
-                tmp.push({ text: str, indent })
+// 自己来写一个简易版 JSON.stringify()
+
+function $JSON_stringify (jsondata, split) {
+    // html string
+    let tmp = ''
+    // 当前迭代器深度
+    let deep = 0
+    if (Array.isArray(jsondata)) {
+        tmp += '['
+        deep++
+        parser(jsondata)
+        deep--
+        tmp += '\n]'
+    } else if (jsondata instanceof Object) {
+        tmp += '{'
+        deep++
+        parser(jsondata)
+        deep--
+        tmp += '\n}'
+    }
+
+    function parser (acc) {
+        const space = _getSpace()
+        for (const key in acc) {
+            if (Array.isArray(acc[key])) {
+                deep++
+                tmp += space + `"${key}": [`
+                parser(acc[key])
+                tmp += space + ']'
+                deep--
             }
-        } else {
-            if (isArray(obj[key])) {
-                if (isPureArray(obj[key])) {
-                    tmp.push({ text: `"${key}": ${parsePureArray(obj[key])}`, indent })
+            else if (acc[key] instanceof Object) {
+                deep++
+                tmp += space + `"${key}": {`
+                parser(acc[key])
+                tmp += space + '}'
+                deep--
+            }
+            else {
+                if (Array.isArray(acc)) {
+                    tmp += space + acc[key]
                 } else {
-                    tmp.push({ text: `"${key}": [`, indent })
-                    tmp = tmp.concat(parseArray(obj[key], indent))
-                    tmp.push({ text: ']', indent })
-                }
-            } else if (obj[key] instanceof Object) {
-                tmp.push({ text: `"${key}": {`, indent })
-                tmp = tmp.concat(parseObject(obj[key], indent))
-                tmp.push({ text: '}', indent })
-            } else {
-                let str = ''
-                if (typeof obj[key] === 'string') {
-                    str = `"${key}": "${obj[key]}"`
-                } else {
-                    str = `"${key}": ${obj[key]}`
-                }
-                tmp.push({ text: str, indent })
+                    tmp += space + `"${key}"` + ': ' + acc[key]
+                }  
             }
         }
     }
     return tmp
-}
-/**
- * 不关心外部"[]"
- * 嵌套数组的解析
- * @param {array} arr
- * @param {number} currentIndent
- */
-function parseArray(arr, currentIndent) {
-    let indent = currentIndent + 4
-    let tmp = []
-    arr.forEach((item, index, arr) => {
-        if (index < arr.length - 1) {
-            // need comma
-            if (isArray(item)) {
-                // item 是数组
-                if (isPureArray(item)) {
-                    tmp.push({ text: parsePureArray(item) + ',', indent })
-                } else {
-                    tmp.push({ text: '[', indent })
-                    tmp = tmp.concat(parseArray(item, indent))
-                    tmp.push({ text: '],', indent })
-                }
-            } else if (item instanceof Object) {
-                // item 是对象
-                tmp.push({ text: '{', indent })
-                tmp = tmp.concat(parseObject(item, indent))
-                tmp.push({ text: '},', indent })
-            } else {
-                // item 为简单value
-                let i = item
-                if (typeof item === 'string') {
-                    i = `"${item}",`
-                } else {
-                    i = `${item},`
-                }
-                tmp.push({ text: i, indent })
-            }
-        } else {
-            if (isArray(item)) {
-                if (isPureArray(item)) {
-                    tmp.push({ text: parsePureArray(item), indent })
-                } else {
-                    tmp.push({ text: '[', indent })
-                    tmp = tmp.concat(parseArray(item, indent))
-                    tmp.push({ text: ']', indent })
-                }
-            } else if (item instanceof Object) {
-                tmp.push({ text: '{', indent })
-                tmp = tmp.concat(parseObject(item, indent))
-                tmp.push({ text: '}', indent })
-            } else {
-                let i = item
-                if (typeof item === 'string') {
-                    i = `"${item}"`
-                }
-                tmp.push({ text: i, indent })
-            }
-        }
-    })
-    return tmp
-}
 
-/**
- * 简单数组,以字符串形式解析
- * @param {Array} arr
- */
-function parsePureArray(arr) {
-    let t = '['
-    arr.forEach((item, index) => {
-        let i = item
-        if (typeof item === 'string') {
-            i = `"${item}"`
+    function _getSpace () {
+        let indence = '\n'
+        let i = 0;
+        while (i++ < deep) {
+            indence += split
         }
-        if (index !== 0) {
-            t += ', ' + i
-        } else {
-            t += i
-        }
-    })
-    t += ']'
-    return t
-}
-/**
- * 判断是否为简单数组
- * @param {array} arr
- */
-function isPureArray(arr) {
-    if (!isArray(arr)) {
-        return false
+        return  indence
     }
-    return arr.every((item) => {
-        return !isArray(item) && !(item instanceof Object)
-    })
-}
-
-function isArray(o) {
-    return Object.prototype.toString.call(o) === '[object Array]'
-}
-
-module.exports = function parseJson(formdata) {
-    var tmp = []
-    if (isArray(formdata)) {
-        tmp.push({ text: '[', indent: 0 })
-        tmp.concat(parseArray(formdata, 0))
-        tmp.push({ text: ']', indent: 0 })
-    } else if (formdata instanceof Object) {
-        tmp.push({ text: '{', indent: 0 })
-        tmp = tmp.concat(parseObject(formdata, 0))
-        tmp.push({ text: '}', indent: 0 })
-    }
-    return tmp
 }
--- readme.md
+++ readme.md
@@ -1,50 +1,6 @@
-## 格式化输出json对象
+# json formatter
 
-### 输出形式
 ```
-[ { text: '{', indent: 0 },
-  { text: '"config": {', indent: 4 },
-  { text: '"batch.size": 10000,', indent: 8 },
-  { text: '"benchmark.interval": "60000",', indent: 8 },
-  { text: '"connect.redis.sink.max.retries": 10,', indent: 8 },
-  { text: '"connection.host": "6.3.7.9",', indent: 8 },
-  { text: '"connection.port": 6379,', indent: 8 },
-  { text: '"connector.class": [1, 2, "test", null]', indent: 8 },
-  { text: '},', indent: 4 },
-  { text: '"connector": {', indent: 4 },
-  { text: '"state": "RUNNING",', indent: 8 },
-  { text: '"worker_id": "9.1.83.2:8083"', indent: 8 },
-  { text: '},', indent: 4 },
-  { text: '"tasks": [', indent: 4 },
-  { text: '1,', indent: 8 },
-  { text: '"test",', indent: 8 },
-  { text: '[1, 2, "arr"],', indent: 8 },
-  { text: '{', indent: 8 },
-  { text: '"id": 0,', indent: 12 },
-  { text: '"state": "RUNNING",', indent: 12 },
-  { text: '"worker_id": "9.1.83.3:8083"', indent: 12 },
-  { text: '},', indent: 8 },
-  { text: '{', indent: 8 },
-  { text: '"id": 1,', indent: 12 },
-  { text: '"state": "RUNNING",', indent: 12 },
-  { text: '"worker_id": "9.1.83.1:8083"', indent: 12 },
-  { text: '}', indent: 8 },
-  { text: ']', indent: 4 },
-  { text: '}', indent: 0 } ]
+const str = JSON.stringify(jsondata, null, 4)
+document.querySelector('pre').innerHTML = str
 ```
-
-### 使用
-参见demo.js
-```
-let parseJson = require('./index.js')
-let output = parseJson(someJsonData)
-```
-
-以在vue中使用为例:
-```
-p(v-for="value of dataCollection", :style="{paddingLeft: value.indent * 8 +'px'}") {{value.text}}
-```
-![demo.png](./demo.png)
-
-### 注意
-需要ES6的支持
